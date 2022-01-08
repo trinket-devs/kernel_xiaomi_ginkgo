2c0e3061bf8640a17ef85224a2683b4bc5bb07c7
LeCmnGend <46700663+LeCmnGend@users.noreply.github.com>
2022-01-08 18:21:36 +0700

    Boeffla 1.1.0
    1.1.0

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -25,9 +25,11 @@
 
 #include "power.h"
 
+//Boeffla config 1.1.0
 #ifdef CONFIG_BOEFFLA_WL_BLOCKER
-	char list_wl[0];
-	char list_wl_search[257] = {0};
+	#include "boeffla_wl_blocker.h"
+	char list_wl_search[LENGTH_LIST_WL_SEARCH] = {0};
+
 	bool wl_blocker_active = false;
 	bool wl_blocker_debug = false;
 	static void wakeup_source_deactivate(struct wakeup_source *ws);
@@ -615,7 +617,7 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 static bool check_for_block(struct wakeup_source *ws)
 {
 	char wakelock_name[52]= {0};
-
+	int length;
 	// if debug mode on, print every wakelock requested
 	if (wl_blocker_debug)
 		printk("Boeffla WL blocker: %s requested\n", ws->name);
@@ -628,8 +630,9 @@ static bool check_for_block(struct wakeup_source *ws)
 	//1.0.1 only if ws structure is valid
 	if (ws)
 	{
-		// wake lock names which are longer than 50 chars are not handled
-		if (strlen(ws->name) > 50)
+		// wake lock names handled have maximum length=50 and minimum=1 1.1.0
+		length = strlen(ws->name);
+		if ((length > 50) || (length < 1))
 			return false;
 		// check if wakelock is in wake lock list to be blocked
 		sprintf(wakelock_name, ";%s;", ws->name);
